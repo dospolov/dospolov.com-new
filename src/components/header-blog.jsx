2d6c53f1b1416a5d4215236750686a7fbf19f9a0
dospolov <marat.dospolov@gmail.com>
2022-09-04 08:39:42 +0700

    header updated

--- src/components/header-blog.jsx
+++ src/components/header-blog.jsx
@@ -1,6 +1,6 @@
 export default function HeaderBlog() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <h2 className="text-xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <a href={'/'} className="hover:underline">
         Blog
       </a>
