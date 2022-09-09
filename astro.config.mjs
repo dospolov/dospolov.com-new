24b31b25b638d0292edce7de31dabb77580f6112
dospolov <marat.dospolov@gmail.com>
2022-09-09 13:03:51 +0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   trailingSlash: 'never'
 })
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const SITE = {
+  // Your site's detail?
+  name: 'Ink',
+  title: 'Astro - Ink',
+  description: 'Crisp, minimal, personal blog theme for Astro',
+  url: 'https://astro-ink.vercel.app',
+  githubUrl: 'https://github.com/one-aalam/astro-ink',
+  listDrafts: true
+  // description ?
+}
