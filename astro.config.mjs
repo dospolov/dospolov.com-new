dc13cf500f9005a41fd2f854506ad1e4c2d9f994
dospolov <marat.dospolov@gmail.com>
2022-09-09 13:26:48 +0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
-  trailingSlash: 'never'
+  trailingSlash: 'never',
+  sitemap: true,
+  site: 'https://dospolov.com'
 })
